8a031fca68fad7567b2849f89bc6459c8707044d
Race Acheson <raceacheson@Races-MacBook-Pro.local>
2019-04-19 10:14:35 -0600

    setting up server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+//server side
+
+const express = require('express')
+const appCtrl = require('./controllers/appCtrl')
+
+const app = express()
+
+
+const massive = require('massive')
+require('dotenv').config()
+
+app.use(express.json())
+
+const { SERVER_PORT, CONNECTION_STRING} = process.env
+
+massive(CONNECTION_STRING).then((dbInstance) => {
+  app.set('db',dbInstance)
+})
+
+
+app.listen(SERVER_PORT, () => {
+  console.log('listening on port:',SERVER_PORT)
+}) 
